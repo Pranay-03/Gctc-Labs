cce452eb8e01e5e2d39008bf022ce024426e5e21
Arigela Pranay kumar <84457467+Pranay-03@users.noreply.github.com>
2023-05-07 12:24:45 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,20 @@
+const fileList = document.getElementById("fileList");
+
+fetch('.')
+  .then(response => response.text())
+  .then(text => {
+    const parser = new DOMParser();
+    const html = parser.parseFromString(text, 'text/html');
+    const links = html.querySelectorAll('a');
+
+    links.forEach(link => {
+      if (link.href.endsWith('.html') || link.href.endsWith('.css') || link.href.endsWith('.js')) {
+        const li = document.createElement('li');
+        const a = document.createElement('a');
+        a.href = link.href;
+        a.innerText = link.href.split('/').pop();
+        li.appendChild(a);
+        fileList.appendChild(li);
+      }
+    });
+  });
