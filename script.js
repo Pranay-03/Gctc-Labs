c0bee49c918484d56aef2308740fe4b1aef64b14
Arigela Pranay kumar <84457467+Pranay-03@users.noreply.github.com>
2023-05-07 12:32:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -8,11 +8,14 @@ fetch('.')
     const links = html.querySelectorAll('a');
 
     links.forEach(link => {
-      if (! link.href.endsWith('.html') || ! link.href.endsWith('.css') || ! link.href.endsWith('.js')) {
+      const href = link.href;
+      const fileType = href.split('.').pop();
+      if (!['html', 'css', 'js'].includes(fileType)) {
         const li = document.createElement('li');
         const a = document.createElement('a');
-        a.href = link.href;
-        a.innerText = link.href.split('/').pop();
+        a.href = href;
+        a.target = "_blank"; // opens the link in a new tab
+        a.innerText = href.split('/').filter(s => s !== '').pop(); // removes empty segments and gets the last non-empty segment
         li.appendChild(a);
         fileList.appendChild(li);
       }
